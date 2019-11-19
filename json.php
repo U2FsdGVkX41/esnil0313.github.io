21fde2d4eb47ed5ee23b8825749bb1d48f903c45
esnil0313 <57315670+esnil0313@users.noreply.github.com>
2019-11-20 05:46:29 +0900

    Update json.php

--- json.php
+++ json.php
@@ -3,8 +3,9 @@
 $num = $_GET["n"];
 $arr = array();
 for($i = 0; $i < $num; $i++) {
-  $src = rand(1, 60);
-  $size = getimagesize("img/" . $src . ".jpg");
+  $picname=array("DSC3963","P1000110");
+  $src = $picname[rand(1, 2)];
+  $size = getimagesize("image/" . $src . ".jpg");
   $height = $size[1];
   if($size[0] != 190) {
     $height = 190 * $size[1] / $size[0];
